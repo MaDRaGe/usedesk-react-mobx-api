3f6d54ee57d8b2201ad3ccaa0117a8c086e54922
MaDRaGe <locodi111@mail.ru>
2020-09-08 13:44:38 +0300

    Updates

---
+++ precache-manifest.e17b7375400257cc527acfe209d6e3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4444937f76117e9038536d8c39d35f60",
+    "url": "/usedesk-react-mobx-api/index.html"
+  },
+  {
+    "revision": "55238474e1f1cd52769a",
+    "url": "/usedesk-react-mobx-api/static/css/main.336d09ff.chunk.css"
+  },
+  {
+    "revision": "8f02387c07d686045f42",
+    "url": "/usedesk-react-mobx-api/static/js/2.13456fa4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/usedesk-react-mobx-api/static/js/2.13456fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55238474e1f1cd52769a",
+    "url": "/usedesk-react-mobx-api/static/js/main.8366121a.chunk.js"
+  },
+  {
+    "revision": "5ae2ef93b2feda1de48a",
+    "url": "/usedesk-react-mobx-api/static/js/runtime-main.3a70becb.js"
+  }
+]);
